35440dfb71c0073aa049ac598de8d3845c211d35
rodrigoschemes <rodrigoschemes@yahoo.com.br>
2017-12-20 00:36:43 -0200

    first commit

---
+++ aspnet-estoqueMvc/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="login">caelum</param>
+        /// <param name="senha">caelum</param>
+        /// <returns></returns>
+        public ActionResult Autentica(String login, String senha)
+        {
+            UsuariosDAO dao = new UsuariosDAO();
+            Usuario usuario = dao.Busca(login, senha);
+
+            if(usuario != null)
+            {
+                Session["usuarioLogado"] = usuario;
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
+}
---
+++ aspnet-estoqueMvc/Models/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CaelumEstoque.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public String Nome { get; set; }
+
+        [Range(0.0, 10000.0)]
+        public float Preco { get; set; }
+
+        public CategoriaDoProduto Categoria { get; set; }
+
+        public int? CategoriaId { get; set; }
+
+        public String Descricao { get; set; }
+
+        public int Quantidade { get; set; }
+    }
+}
